93065f19db72b880ebc9c2602fe0274afc78d23b
System Administrator <root@iMac-de-iMac21.local>
2019-01-12 12:40:16 -0300

    loading files

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React,{ Component } from 'react';
+import Clock from './Clock';
+import './App.css'
+
+class App extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			deadline: 'December 25, 2019',
+			newDeadline:''
+		}
+		this.changeDeadline= this.changeDeadline.bind(this)
+	}
+
+	changeDeadline(){
+		this.setState({
+			deadline: this.state.newDeadline
+		})
+	}
+
+	render(){
+		return(
+			<div className="App">
+				<div className="App-title">Countdown to {this.state.deadline}</div>
+					<Clock 
+					deadline={this.state.deadline}/>
+				<input 
+					placeholder='new date' 
+					onChange={event =>this.setState({newDeadline:event.target.value})}/>
+				<button onClick={ () => this.changeDeadline() }>Submit</button>
+				
+			</div>
+		)
+	}
+}
+
+export default App
